754a8f1de1016b8e50057917c2a7a80b6ae56a99
lcmftianci <707010543@qq.com>
2017-09-03 20:40:55 +0800

    add joystick

---
+++ Assets/bird.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class bird : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+		if (Input.GetKeyDown (KeyCode.W)) {
+			Debug.Log("print Up");
+						transform.position += Vector3.up;		
+				} else if (Input.GetKeyDown (KeyCode.S)) {
+			Debug.Log("print Down");
+			transform.position += Vector3.down;		
+		}
+	}
+}
